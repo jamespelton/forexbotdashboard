7a1d509e1cf6d9ff658bc86504ec552f9bf7a8e7
jamespelton <123276113+jamespelton@users.noreply.github.com>
2023-02-05 14:15:37 -0600

    Insert into the DB

--- get_oanda_balance.php
+++ get_oanda_balance.php
@@ -29,6 +29,27 @@ if ($err) {
 
 $data = json_decode($response);
 
-echo $data->account->balance;
+$servername = "localhost";
+$username = "root";
+$password = "imaSSJ722!";
+$dbname = "ForexNFTs";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO Balances (balance, account) VALUES ('{$data->account->balance}', 'oanda')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 
 ?>
