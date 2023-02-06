d865b66ebb81f78a732255e56e030be54c53f3cb
jamespelton <123276113+jamespelton@users.noreply.github.com>
2023-02-06 12:40:42 -0600

    Update get_adrofx_balance.php

--- get_adrofx_balance.php
+++ get_adrofx_balance.php
@@ -1,7 +1,10 @@
 <?php
-$account1 = $_GET['account1'];
-$account2 = $_GET['account2'];
-$balance = $account1+$account2;
+
+//Get data from Browse AI
+$teamID = "9b93e61d-1842-4407-8990-7e88a31b74e4";
+$robotID = "aa894ab2-b635-4b7a-b474-f0dd34da30d7";
+$secretKey = "61fe6a77-84c9-43ac-885d-64797529ca78:e8f5c831-daf7-459d-91e4-a432c3e12d6a";
+
 
 $servername = "localhost";
 $username = "jpelton";
@@ -10,9 +13,9 @@ $dbname = "ForexNFTs";
 
 $dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-$request = print_r($_REQUEST,true);
+$request = print_r($_POST,true);
 
-$query = "INSERT INTO log (log_entry, log_time) VALUES ('$request', NOW());";
+$query = "INSERT INTO log (log_entry, log_time) VALUES ('{$request}', NOW());";
 
 // Run your query
 $sth = $dbh->prepare($query);
