48e61f094ccedd326d4252a8798d7007aef20f4a
jamespelton <123276113+jamespelton@users.noreply.github.com>
2023-02-06 14:45:35 -0600

    Update get_adrofx_balance.php

--- get_adrofx_balance.php
+++ get_adrofx_balance.php
@@ -6,6 +6,41 @@ $robotID = "aa894ab2-b635-4b7a-b474-f0dd34da30d7";
 $secretKey = "61fe6a77-84c9-43ac-885d-64797529ca78:e8f5c831-daf7-459d-91e4-a432c3e12d6a";
 
 
+//Get a list of all tasks in browse.ai
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api.browse.ai/v2/robots/{$robotID}/tasks?page=1",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "Authorization: Bearer $secretKey"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+	$result = json_decode($response)->result;
+	$robotTasks = $result->robotTasks;
+	$items = $robotTasks->items;
+	$last_item = end($items);
+	$balance1 = str_replace('USD','',str_replace(' ','',$last_item->capturedTexts->{'220600 Balance'}));
+	$balance2 = str_replace('USD','',str_replace(' ','',$last_item->capturedTexts->{'220628 Balance'}));
+	$total_balance = $balance1 + $balance2;
+}
+
+
+//Put in the db
 $servername = "localhost";
 $username = "jpelton";
 $password = "imaSSJ722!";
@@ -15,7 +50,7 @@ $dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
 $request = print_r($_POST,true);
 
-$query = "INSERT INTO log (log_entry, log_time) VALUES ('{$request}', NOW());";
+$query = "INSERT INTO Balances (balance, account) VALUES ('$total_balance', 'adrofx')";
 
 // Run your query
 $sth = $dbh->prepare($query);
