747af40ffc49a23c9669fd0e4abe42f01a0b94b5
jamespelton <123276113+jamespelton@users.noreply.github.com>
2023-02-06 11:58:46 -0600

    Update get_adrofx_balance.php

--- get_adrofx_balance.php
+++ get_adrofx_balance.php
@@ -3,4 +3,20 @@ $account1 = $_GET['account1'];
 $account2 = $_GET['account2'];
 $balance = $account1+$account2;
 
-echo "$balance\n";
+$servername = "localhost";
+$username = "root";
+$password = "imaSSJ722!";
+$dbname = "ForexNFTs";
+
+$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+$query = "INSERT INTO log (log_entry, log_time) VALUES (print_r($_REQUEST,true), NOW());";
+
+// Run your query
+$sth = $dbh->prepare($query);
+$sth->execute();
+
+// Close connection
+$dbh = null;
+
+echo "Finished\n";
