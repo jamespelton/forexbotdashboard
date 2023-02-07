751d90988965d93c0e15a316aea58358e781a98e
jamespelton <jpelton722@gmail.com>
2023-02-06 19:44:07 -0600

    Create daily_results.php

---
+++ daily_results.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "jpelton";
+$password = "imaSSJ722!";
+$dbname = "ForexNFTs";
+
+$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+
+$sql = "SELECT * FROM Balances";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
+//Loop Through Result Set
+while ($row = $stmt->fetch()) {
+    //Calculate Daily Return
+    $date = $row['date'];
+    $previousValue = $row['value'];
+    $dailyReturn = ($previousValue - $row['value']) / $previousValue;
+    $dailyReturn = $dailyReturn * 100;
+    
+    //Display Daily Return on Calendar
+    echo "<td>$date: <br>$dailyReturn%</td>";
+}
