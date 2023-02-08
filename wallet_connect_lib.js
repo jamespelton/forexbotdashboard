e22758a1552b9a804584769b5670cf57a591ed2a
jamespelton <jpelton722@gmail.com>
2023-02-07 21:47:37 -0600

    Update wallet_connect_lib.js

--- wallet_connect_lib.js
+++ wallet_connect_lib.js
@@ -1,29 +1,32 @@
-import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi/connector'
-import { configureChains, createClient } from '@wagmi/core'
-import { goerli, mainnet } from '@wagmi/core/chains'
-import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/html'
+import { configureChains, createClient } from "@wagmi/core";
+import { arbitrum, mainnet, polygon } from "@wagmi/core/chains";
+import { Web3Modal } from "@web3modal/html";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
 
-// 1. Define constants
-const projectId = '8e6b5ffdcbc9794bf9f4a1952578365b'
-const chains = [mainnet, goerli]
+const chains = [arbitrum, mainnet, polygon];
 
-// 2. Configure wagmi client
-const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
+// Wagmi Core Client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "66de21812516e1791389bdebf415aced" }),
+]);
 const wagmiClient = createClient({
   autoConnect: true,
-  connectors: [...modalConnectors({ appName: 'web3Modal', chains }), new SafeConnector({ chains })],
-  provider
-})
+  connectors: modalConnectors({
+    projectId: "66de21812516e1791389bdebf415aced",
+    version: "1", // or "2"
+    appName: "web3Modal",
+    chains,
+  }),
+  provider,
+});
 
-// 3. Create ethereum and modal clients
-const ethereumClient = new EthereumClient(wagmiClient, chains)
-export const web3Modal = new Web3Modal(
-  {
-    projectId,
-    walletImages: {
-      safe: 'https://pbs.twimg.com/profile_images/1566773491764023297/IvmCdGnM_400x400.jpg'
-    }
-  },
+// Web3Modal and Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+const web3modal = new Web3Modal(
+  { projectId: "66de21812516e1791389bdebf415aced" },
   ethereumClient
-)
+);
